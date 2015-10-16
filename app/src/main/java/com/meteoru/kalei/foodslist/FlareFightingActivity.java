9f40053e98013ca1ff2729f33f206cc384d2c7c1
C K Ashby <clay.ashby@gmail.com>
2015-10-16 16:00:17 -0700

    Renamed FoodList and page titles

--- app/src/main/java/com/meteoru/kalei/foodslist/FlareFightingActivity.java
+++ app/src/main/java/com/meteoru/kalei/foodslist/FlareFightingActivity.java
@@ -5,9 +5,14 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.Spanned;
 import android.view.View;
+import android.widget.TextView;
 
 public class FlareFightingActivity extends AppCompatActivity {
+    TextView tvFlareFightingIntro;
+    TextView tvFlareFightingHtml;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +25,21 @@ public class FlareFightingActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Add a Note or Recipe?", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        tvFlareFightingIntro = (TextView) findViewById(R.id.tvFlareFightingIntro);
+        tvFlareFightingHtml = (TextView) findViewById(R.id.tvFlareFightingHtml);
+
+        String htmlAsString = getString(R.string.flare_fighting_foods_html);
+        Spanned htmlAsSpanned = Html.fromHtml(htmlAsString);
+
+        tvFlareFightingHtml.setText(htmlAsSpanned);
+
+
     }
 
 }
