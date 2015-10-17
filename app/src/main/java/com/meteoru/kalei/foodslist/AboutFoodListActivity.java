c1cf87a35a3035e3c44ab49fc88a09a2dd9148bd
C K Ashby <clay.ashby@gmail.com>
2015-10-16 17:03:19 -0700

    Created green text link to icn Web site in About Our List

--- app/src/main/java/com/meteoru/kalei/foodslist/AboutFoodListActivity.java
+++ app/src/main/java/com/meteoru/kalei/foodslist/AboutFoodListActivity.java
@@ -1,16 +1,21 @@
 package com.meteoru.kalei.foodslist;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.text.Spanned;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class AboutFoodListActivity extends AppCompatActivity {
     TextView tvAboutFoodListIntro;
     TextView tvAboutFoodListHtml;
     TextView tvAboutFoodListHtml2;
+    Button btWebICNetwork;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +28,20 @@ public class AboutFoodListActivity extends AppCompatActivity {
         tvAboutFoodListIntro = (TextView) findViewById(R.id.tvAboutFoodListIntro);
         tvAboutFoodListHtml = (TextView) findViewById(R.id.tvAboutFoodListHtml);
         tvAboutFoodListHtml2 = (TextView) findViewById(R.id.tvAboutFoodListHtml2);
+        btWebICNetwork = (Button) findViewById(R.id.btWebICNetwork);
 
         String htmlAsString = getString(R.string.about_food_list_html);
         Spanned htmlAsSpanned = Html.fromHtml(htmlAsString);
         tvAboutFoodListHtml.setText(htmlAsSpanned);
+
+        btWebICNetwork.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://www.ic-network.com";
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+            }
+        });
+
 
         String htmlAsString2 = getString(R.string.about_food_list_html2);
         Spanned htmlAsSpanned2 = Html.fromHtml(htmlAsString2);
