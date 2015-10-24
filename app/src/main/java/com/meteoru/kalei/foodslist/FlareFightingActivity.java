9934e6246fd10bde2ba880197f803867e4b0b452
C K Ashby <clay.ashby@gmail.com>
2015-10-24 15:02:26 -0700

    Added dimens_margin_vertical to Flare Fighting Foods

--- app/src/main/java/com/meteoru/kalei/foodslist/FlareFightingActivity.java
+++ app/src/main/java/com/meteoru/kalei/foodslist/FlareFightingActivity.java
@@ -1,13 +1,10 @@
 package com.meteoru.kalei.foodslist;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.text.Spanned;
-import android.view.View;
 import android.widget.TextView;
 
 public class FlareFightingActivity extends AppCompatActivity {
@@ -20,15 +17,6 @@ public class FlareFightingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_flare_fighting);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Add a Note or Recipe?", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         tvFlareFightingIntro = (TextView) findViewById(R.id.tvFlareFightingIntro);
@@ -36,10 +24,7 @@ public class FlareFightingActivity extends AppCompatActivity {
 
         String htmlAsString = getString(R.string.flare_fighting_foods_html);
         Spanned htmlAsSpanned = Html.fromHtml(htmlAsString);
-
         tvFlareFightingHtml.setText(htmlAsSpanned);
-
-
     }
 
 }
