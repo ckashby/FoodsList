80181341389e059c87ec59eeb6482215d072f6a2
C K Ashby <clay.ashby@gmail.com>
2015-10-25 15:44:31 -0700

    Add Landscape

---
+++ app/src/main/java/com/meteoru/kalei/foodslist/DetailsFragment.java
@@ -0,0 +1,73 @@
+package com.meteoru.kalei.foodslist;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DetailsFragment extends Fragment {
+    TextView tvName;
+    TextView tvDescription;
+    TextView tvFriendlyTitle;
+    TextView tvFriendlyText;
+    TextView tvTryItTitle;
+    TextView tvTryItText;
+    TextView tvCautionTitle;
+    TextView tvCautionText;
+
+    Food food;
+
+    public static Fragment newInstance(Food foodItem){
+        Fragment detailsFragment = new DetailsFragment();
+        Bundle args = new Bundle();
+        args.putParcelable(ProductMenuActivity.ITEM_EXTRA, foodItem);
+        detailsFragment.setArguments(args);
+        return detailsFragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        food = getArguments().getParcelable(ProductMenuActivity.ITEM_EXTRA);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View layout = inflater.inflate(R.layout.content_product_menu, container, false);
+        tvName = (TextView) layout.findViewById(R.id.tvName);
+        tvDescription = (TextView) layout.findViewById(R.id.tvDescription);
+        tvFriendlyTitle = (TextView) layout.findViewById(R.id.tvFriendlyTitle);
+        tvFriendlyText = (TextView) layout.findViewById(R.id.tvFriendlyText);
+        tvTryItTitle = (TextView) layout.findViewById(R.id.tvFriendlyTitle);
+        tvTryItText = (TextView) layout.findViewById(R.id.tvTryItText);
+        tvCautionTitle = (TextView) layout.findViewById(R.id.tvCautionTitle);
+        tvCautionText = (TextView) layout.findViewById(R.id.tvCautionText);
+
+        tvName.setText(food.getName());
+        tvDescription.setText(food.getDescription());
+        tvFriendlyText.setText(food.getFriendly());
+        tvTryItText.setText(food.getTryit());
+        tvCautionText.setText(food.getCaution());
+
+        if (food.getFriendly() == null || food.getFriendly().trim().isEmpty()){
+            tvFriendlyTitle.setVisibility(TextView.GONE);
+            tvFriendlyText.setVisibility(TextView.GONE);
+        }
+
+        if (food.getTryit() == null || food.getTryit().trim().isEmpty()){
+            tvTryItTitle.setVisibility(TextView.GONE);
+            tvTryItText.setVisibility(TextView.GONE);
+        }
+
+        if (food.getCaution() == null || food.getCaution().trim().isEmpty()){
+            tvCautionTitle.setVisibility(TextView.GONE);
+            tvCautionText.setVisibility(TextView.GONE);
+        }
+
+        return layout;
+    }
+}
--- app/src/main/java/com/meteoru/kalei/foodslist/ProductMenuActivity.java
+++ app/src/main/java/com/meteoru/kalei/foodslist/ProductMenuActivity.java
@@ -1,13 +1,16 @@
 package com.meteoru.kalei.foodslist;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
 import android.widget.ListView;
 
 import java.util.List;
@@ -21,17 +24,23 @@ public class ProductMenuActivity extends AppCompatActivity {
     ListView lvProducts;
     ArrayAdapter<Food> aLvProducts;
     DbHelper dbHelper;
+    FrameLayout detailsContainer;
 
     @Override
-    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+    public void onSaveInstanceState(Bundle outState) {
         outState.putString(CATEGORY_EXTRA, category);
-        super.onSaveInstanceState(outState, outPersistentState);
+        if (category == null){
+            Log.d("ProductMenuActivity", "Category is Null");
+        }
+        Log.d("ProductMenuActivity", "outState Function was called");
+        super.onSaveInstanceState(outState);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_menu);
+        detailsContainer = (FrameLayout) findViewById(R.id.detailsContainer);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -41,6 +50,7 @@ public class ProductMenuActivity extends AppCompatActivity {
             category = getIntent().getStringExtra(CATEGORY_EXTRA);
         } else {
             category = savedInstanceState.getString(CATEGORY_EXTRA);
+            Log.d("ProductMenuActivity", category.toString());
         }
         dbHelper = new DbHelper(this);
         productList = dbHelper.getFoodsByCategory(category);
@@ -48,16 +58,32 @@ public class ProductMenuActivity extends AppCompatActivity {
         aLvProducts = new ArrayAdapter<Food>(
                 this, R.layout.food_item_layout, R.id.tvFoodListView, productList);
         lvProducts.setAdapter(aLvProducts);
-        lvProducts.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Food food = productList.get(position);
-                Intent intent = new Intent(ProductMenuActivity.this, DetailsActivity.class);
-                intent.putExtra(ITEM_EXTRA, food);
-                startActivity(intent);
-            }
-        });
-
+        if (detailsContainer == null) {
+            lvProducts.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Food food = productList.get(position);
+                    Intent intent = new Intent(ProductMenuActivity.this, DetailsActivity.class);
+                    intent.putExtra(ITEM_EXTRA, food);
+                    startActivity(intent);
+                }
+            });
+        } else {
+            lvProducts.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    selectItem(position);
+                }
+            });
+            selectItem(0);
+        }
     }
 
+    private void selectItem(int position){
+        Food food = productList.get(position);
+        Fragment detailsFragment = DetailsFragment.newInstance(food);
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.detailsContainer, detailsFragment);
+        ft.commit();
+    }
 }
